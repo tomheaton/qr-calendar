e716c9497c6938646aef83c66f2da288ce405408
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-20 23:32:06 +0000

    cleanup :broom:

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,15 +1,6 @@
 import styles from "../styles/Footer.module.css";
-//import {toggleTheme as toggleThemeOld, getTheme} from "../utils/theme";
 
 const Footer = ({toggleTheme, currentTheme}: {toggleTheme: any, currentTheme: any}) => {
-
-    // let icon = currentTheme === "dark" ? "bi-moon-fill" : "bi-sun-fill";
-/*    let icon = getTheme() === "dark" ? "bi-moon-fill" : "bi-sun-fill";
-
-    const handleThemeToggle = () => {
-        toggleThemeOld()
-        icon = getTheme() === "dark" ? "bi-moon-fill" : "bi-sun-fill";
-    }*/
 
     return (
         <footer className={styles.footer}>
@@ -17,9 +8,6 @@ const Footer = ({toggleTheme, currentTheme}: {toggleTheme: any, currentTheme: an
                 Tom Heaton
             </a>
             <button onClick={toggleTheme}>
-            {/*<button onClick={handleThemeToggle}>*/}
-            {/*    <i className={`bi ${icon} ${styles.themeButton}`} aria-label="Theme Toggle"/>*/}
-            {/*    <i className={`bi ${currentTheme === "dark" ? "bi-moon-fill" : "bi-sun-fill"} ${styles.themeButton}`} aria-label="Theme Toggle"/>*/}
                 <i className={`bi ${currentTheme == "dark" ? "bi-moon-fill" : "bi-sun-fill"} ${styles.themeButton}`} aria-label="Theme Toggle"/>
             </button>
             <a target="_blank" href="https://www.github.com/tomheaton/qr-calendar" rel="noopener noreferrer">
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,16 +3,6 @@ import {toggleTheme, getTheme} from "../utils/theme";
 
 const Layout = ({children}: { children: any }) => {
 
-    // TODO: move this to _app.tsx
-
-/*    let currentTheme: string = "light";
-
-    const toggleTheme = () => {
-        console.log("toggle theme");
-        currentTheme = currentTheme === "light" ? "dark" : "light";
-        console.log("theme is " + currentTheme);
-    }*/
-
     return (
         <>
             {/*<Navbar />*/}
