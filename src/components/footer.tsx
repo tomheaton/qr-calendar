5c129100b10ed120aa8e760378ed4dd971f5fb63
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-20 23:31:33 +0000

    move functions around

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,24 +1,26 @@
 import styles from "../styles/Footer.module.css";
-import {toggleTheme as toggleThemeOld, getTheme} from "../utils/theme";
+//import {toggleTheme as toggleThemeOld, getTheme} from "../utils/theme";
 
-const Footer = ({toggleTheme, currentTheme}: {toggleTheme: any, currentTheme: string}) => {
+const Footer = ({toggleTheme, currentTheme}: {toggleTheme: any, currentTheme: any}) => {
 
     // let icon = currentTheme === "dark" ? "bi-moon-fill" : "bi-sun-fill";
-    let icon = getTheme() === "dark" ? "bi-moon-fill" : "bi-sun-fill";
+/*    let icon = getTheme() === "dark" ? "bi-moon-fill" : "bi-sun-fill";
 
     const handleThemeToggle = () => {
         toggleThemeOld()
         icon = getTheme() === "dark" ? "bi-moon-fill" : "bi-sun-fill";
-    }
+    }*/
 
     return (
         <footer className={styles.footer}>
             <a target="_blank" href="https://www.tomheaton.dev" rel="noopener noreferrer">
                 Tom Heaton
             </a>
-            {/*<button onClick={toggleTheme}>*/}
-            <button onClick={handleThemeToggle}>
-                <i className={`bi ${icon} ${styles.themeButton}`} aria-label="Theme Toggle"/>
+            <button onClick={toggleTheme}>
+            {/*<button onClick={handleThemeToggle}>*/}
+            {/*    <i className={`bi ${icon} ${styles.themeButton}`} aria-label="Theme Toggle"/>*/}
+            {/*    <i className={`bi ${currentTheme === "dark" ? "bi-moon-fill" : "bi-sun-fill"} ${styles.themeButton}`} aria-label="Theme Toggle"/>*/}
+                <i className={`bi ${currentTheme == "dark" ? "bi-moon-fill" : "bi-sun-fill"} ${styles.themeButton}`} aria-label="Theme Toggle"/>
             </button>
             <a target="_blank" href="https://www.github.com/tomheaton/qr-calendar" rel="noopener noreferrer">
                 View Source
