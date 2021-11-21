902e294a891e82144a66c3b8995ad1468a10acb7
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-21 09:34:44 +0100

    ???

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,20 +3,13 @@ import {useEffect, useState} from "react";
 
 const Footer = ({toggleTheme, currentTheme}: {toggleTheme: any, currentTheme: any}) => {
 
-    const [icon, setIcon] = useState<string>();
-
-    useEffect(() => {
-        setIcon(currentTheme == "dark" ? "bi-moon-fill" : "bi-sun-fill");
-        console.log("changing theme")
-    }, [currentTheme]);
-
     return (
         <footer className={styles.footer}>
             <a target="_blank" href="https://www.tomheaton.dev" rel="noopener noreferrer">
                 Tom Heaton
             </a>
             <button onClick={toggleTheme}>
-                <i className={`bi ${icon} ${styles.themeButton}`} aria-label="Theme Toggle"/>
+                <i className={`bi ${currentTheme == "dark" ? "bi-moon-fill" : "bi-sun-fill"} ${styles.themeButton}`} aria-label="Theme Toggle"/>
             </button>
             <a target="_blank" href="https://www.github.com/tomheaton/qr-calendar" rel="noopener noreferrer">
                 View Source
