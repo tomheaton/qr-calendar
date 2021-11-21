bdcf6e5b81b5ed59081e8188e366b12c9be49ab1
Thomas Heaton <thomas.m.heaton@gmail.com>
2021-11-21 09:21:38 +0100

    state fixes?

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,14 @@
 import styles from "../styles/Footer.module.css";
+import {useEffect, useState} from "react";
 
 const Footer = ({toggleTheme, currentTheme}: {toggleTheme: any, currentTheme: any}) => {
+
+    const [icon, setIcon] = useState<string>();
+
+    useEffect(() => {
+        setIcon(currentTheme == "dark" ? "bi-moon-fill" : "bi-sun-fill");
+        console.log("changing theme")
+    }, [currentTheme]);
 
     return (
         <footer className={styles.footer}>
@@ -8,7 +16,7 @@ const Footer = ({toggleTheme, currentTheme}: {toggleTheme: any, currentTheme: an
                 Tom Heaton
             </a>
             <button onClick={toggleTheme}>
-                <i className={`bi ${currentTheme == "dark" ? "bi-moon-fill" : "bi-sun-fill"} ${styles.themeButton}`} aria-label="Theme Toggle"/>
+                <i className={`bi ${icon} ${styles.themeButton}`} aria-label="Theme Toggle"/>
             </button>
             <a target="_blank" href="https://www.github.com/tomheaton/qr-calendar" rel="noopener noreferrer">
                 View Source
