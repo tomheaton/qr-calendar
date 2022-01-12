322fba052b262efc92f1304958b062b983866beb
Thomas Heaton <thomas.m.heaton@gmail.com>
2022-01-12 20:38:03 +0000

    (fix) footer element widths

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -4,24 +4,42 @@ import {getTheme, toggleTheme} from "../utils/theme";
 
 const Footer = () => {
 
-    const [icon, setIcon] = useState<string>("bi-sun-fill");
+    const [icon, setIcon] = useState<string>("bi-moon-fill");
 
     const handleToggleTheme = () => {
         toggleTheme();
-        setIcon(getTheme() === "dark" ? "bi-sun-fill" : "bi-moon-fill");
+        setIcon(getTheme() === "light" ? "bi-moon-fill" : "bi-sun-fill");
     }
 
     return (
-        <footer className={styles.footer}>
-            <a target="_blank" href="https://www.tomheaton.dev" rel="noopener noreferrer">
-                Tom Heaton
-            </a>
+        // <footer className={styles.footer}>
+        <footer className={`dev ${styles.footer}`}>
+            {/*<div style={{width: "50%"}}>
+                <a target="_blank" href="https://www.tomheaton.dev" rel="noopener noreferrer">
+                    Tom Heaton
+                </a>
+            </div>
             <div onClick={handleToggleTheme} className={styles.themeButton}>
                 <i className={`bi ${icon}`} aria-label="Theme Toggle"/>
             </div>
-            <a target="_blank" href="https://www.github.com/tomheaton/qr-calendar" rel="noopener noreferrer">
-                View Source
-            </a>
+            <div style={{width: "50%"}}>
+                <a target="_blank" href="https://www.github.com/tomheaton/qr-calendar" rel="noopener noreferrer">
+                    View Source
+                </a>
+            </div>*/}
+            <div>
+                <a target="_blank" href="https://www.tomheaton.dev" rel="noopener noreferrer">
+                    Tom Heaton
+                </a>
+            </div>
+            <div onClick={handleToggleTheme} className={styles.themeButton}>
+                <i className={`bi ${icon}`} aria-label="Theme Toggle"/>
+            </div>
+            <div>
+                <a target="_blank" href="https://www.github.com/tomheaton/qr-calendar" rel="noopener noreferrer">
+                    View Source
+                </a>
+            </div>
         </footer>
     );
 }
