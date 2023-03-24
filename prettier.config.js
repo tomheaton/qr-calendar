9ab753420a317c3422e7380f3df2d99848fb5b26
Tom Heaton <thomas.m.heaton@gmail.com>
2023-03-24 17:40:58 +0000

    prettier + editorconfig

---
+++ prettier.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  trailingComma: "all",
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: false,
+  bracketSpacing: true,
+  printWidth: 100,
+  endOfLine: "lf",
+  plugins: [require("prettier-plugin-tailwindcss")],
+  tailwindConfig: "./tailwind.config.js",
+};
