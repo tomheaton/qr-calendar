e37cb694013566923711b42290b9e9e30ee286cf
Tom Heaton <thomas.m.heaton@gmail.com>
2023-06-09 02:56:19 +0100

    remove analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,50 +1,52 @@
-import { pageView } from "@/lib/gtag";
+// import { pageView } from "@/lib/gtag";
 import "@/styles/globals.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import type { AppType } from "next/app";
-import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
 
 const App: AppType = ({ Component, pageProps }) => {
-  const router = useRouter();
+  // const router = useRouter();
 
-  const [loading, setLoading] = useState<boolean>(false);
+  // const [loading, setLoading] = useState<boolean>(false);
 
-  useEffect(() => {
-    const handleStart = () => {
-      setLoading(true);
-    };
+  // useEffect(() => {
+  //   const handleStart = () => {
+  //     setLoading(true);
+  //   };
 
-    const handleComplete = () => {
-      setLoading(false);
-    };
+  //   const handleComplete = () => {
+  //     setLoading(false);
+  //   };
 
-    router.events.on("routeChangeStart", handleStart);
-    router.events.on("routeChangeComplete", handleComplete);
-    router.events.on("routeChangeError", handleComplete);
+  //   router.events.on("routeChangeStart", handleStart);
+  //   router.events.on("routeChangeComplete", handleComplete);
+  //   router.events.on("routeChangeError", handleComplete);
 
-    const handleRouteChange = (url: URL) => {
-      /*window.gtag('config', `${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}`, {
-        page_path: url
-      });*/
-      pageView(url);
-    };
+  //   const handleRouteChange = (url: URL) => {
+  //     /*window.gtag('config', `${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_KEY}`, {
+  //       page_path: url
+  //     });*/
+  //     pageView(url);
+  //   };
 
-    router.events.on("routeChangeComplete", handleRouteChange);
+  //   router.events.on("routeChangeComplete", handleRouteChange);
 
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
+  //   return () => {
+  //     router.events.off("routeChangeComplete", handleRouteChange);
+  //   };
+  // }, [router.events]);
 
-  return loading ? (
-    <>
-      <h1>QR Calendar</h1>
-      <p>loading...</p>
-    </>
-  ) : (
-    <Component {...pageProps} />
-  );
+  // return loading ? (
+  //   <>
+  //     <h1>QR Calendar</h1>
+  //     <p>loading...</p>
+  //   </>
+  // ) : (
+  //   <Component {...pageProps} />
+  // );
+
+  return <Component {...pageProps} />;
 };
 
 export default App;
