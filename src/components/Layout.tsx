d9fc07d6d5fa32b74a97c11199c7d4945d00c46d
Tom Heaton <thomas.m.heaton@gmail.com>
2023-09-04 00:02:22 +0100

    fix

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import type { PropsWithChildren } from "react";
-import Footer from "./Footer";
+import Footer from "@components/footer";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
